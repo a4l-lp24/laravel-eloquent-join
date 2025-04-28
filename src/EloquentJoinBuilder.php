5b50000ca849b11b30f71965edceffd722090a34
majtanmarek <marek.majtan@all4labels.com>
2025-04-28 13:12:07 +0200

    feat: added possiblity for array in primary key

--- src/EloquentJoinBuilder.php
+++ src/EloquentJoinBuilder.php
@@ -244,7 +244,11 @@ class EloquentJoinBuilder extends Builder
         if (!$this->selected && count($relations) > 1) {
             $this->selected = true;
             $this->selectRaw($baseTable.'.*');
-            $this->groupBy($baseTable.'.'.$basePrimaryKey);
+            $keys = is_array($basePrimaryKey) ? $basePrimaryKey : [$basePrimaryKey];
+            foreach($keys AS $key){
+                $this->groupBy($baseTable.'.'.$key);
+            }
+            
         }
 
         return $currentTableAlias.'.'.$column;
